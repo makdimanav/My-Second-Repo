0e3ed9426fe92512149f47600188bf98cafecfd5
makdimanav <69.theaayush@gmail.com>
2024-09-04 12:59:56 +0530

    Create aayu.java
    java file for giving name and age

---
+++ aayu.java
@@ -0,0 +1,15 @@
+public class personn {
+
+	private String name;
+	private int age;
+	public personn(String name, int age ) {
+		this.name=name;
+		this.age=age;
+	}
+	public void display() {
+		System.out.println("name=" + this.name);
+		System.out.println("age=" + this.age);
+		
+	}
+	
+}
